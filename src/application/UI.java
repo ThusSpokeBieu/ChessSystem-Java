746e0a704a77314ca014f557c397c94735753602
Gabriel Messias Costa <gmessiasc@gmail.com>
2022-10-03 18:38:45 -0300

    queen

--- src/application/UI.java
+++ src/application/UI.java
@@ -69,25 +69,25 @@ public class UI {
     public static void printBoard(ChessPiece[][] pieces) {
         System.out.println();
         for(int i = 0; i < pieces.length; i++){
-            System.out.print("\t" + (8 - i) + " ");
+            System.out.print(" " + (8 - i) + " ");
             for(int j = 0; j < pieces.length; j++){
                 printPiece(pieces[i][j], false);
             }
             System.out.println();
         }
-        System.out.println("\t  a b c d e f g h\n");
+        System.out.println("   a b c d e f g h");
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
         System.out.println();
         for(int i = 0; i < pieces.length; i++){
-            System.out.print("\t" + (8 - i) + " ");
+            System.out.print(" " + (8 - i) + " ");
             for(int j = 0; j < pieces.length; j++){
                 printPiece(pieces[i][j], possibleMoves[i][j]);
             }
             System.out.println();
         }
-        System.out.println("\t  a b c d e f g h\n");
+        System.out.println("   a b c d e f g h");
     }
 
     private static void printPiece(ChessPiece piece, boolean background){
