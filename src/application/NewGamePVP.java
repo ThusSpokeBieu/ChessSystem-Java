0c4236f72e2589d22b77319b8fce23b8ae663fc7
Gabriel Messias Costa <gmessiasc@gmail.com>
2022-10-19 19:57:58 -0300

    Fix: better treatment to promotion

--- src/application/NewGamePVP.java
+++ src/application/NewGamePVP.java
@@ -5,6 +5,7 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -39,7 +40,11 @@ public class NewGamePVP {
 
                 if (chessMatch.getPromoted() != null){
                     System.out.println("Enter piece for promotion (B/N/R/Q):");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+                    while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q") ){
+                        System.out.println("Invalid value! Enter a piece for promotion (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
 
